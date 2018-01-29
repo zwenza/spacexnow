e13532add05263410d28d81ba851d303ee781083
David <david.joech@gmail.com>
2018-01-29 18:44:29 +0100

    agency list component

---
+++ src/components/AgencyList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Agencies</h1>
+        <p v-if="!agencies">Loading...</p>
+        <ul>
+          <li v-for="agency in agencies">{{agency.name}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      agencies: undefined
+    };
+  },
+  created() {
+    axios.get(`https://launchlibrary.net/1.3/agency`).then(response => {
+      this.agencies = response.data.agencies;
+    });
+  }
+};
+</script>
