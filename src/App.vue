4b21755d0fdaeb1e14a6d77a22c5faa64f55c2fc
David <david.joech@gmail.com>
2018-02-03 16:59:31 +0100

    added Footer and minor restructuring

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <sp-navigation></sp-navigation>
     <router-view/>
+    <sp-footer></sp-footer>
   </div>
 </template>
 
 <script>
 import Navigation from '@/components/Navigation';
+import Footer from '@/components/Footer';
 
 export default {
   name: 'App',
   components: {
-    'sp-navigation': Navigation
+    'sp-navigation': Navigation,
+    'sp-footer': Footer
   }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import AgencyList from '@/components/AgencyList';
-import LaunchList from '@/components/LaunchList';
+import AgencyList from '@/components/space/AgencyList';
+import LaunchList from '@/components/space/LaunchList';
 
 Vue.use(Router);
 
