c1598ef889a23ff6e51a3ec95f87dd73d16645a6
Florian Kapfenberger <florian@kapfenberger.me>
2018-10-17 21:56:17 +0200

    added logo, minor design changes & updated react + dependencies

--- src/LaunchList.js
+++ src/LaunchList.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Col, Row } from 'antd';
-import Launch from './Launch';
-import withLoadingIndicator from './util/withLoadingIndicator';
+import React from "react";
+import { Col, Row } from "antd";
+import Launch from "./Launch";
+import withLoadingIndicator from "./util/withLoadingIndicator";
 
 const fetchLaunchData = async () => {
-  let response = await fetch('https://api.spacexdata.com/v2/launches/upcoming');
+  let response = await fetch("https://api.spacexdata.com/v2/launches/upcoming");
   return await response.json();
 };
 
@@ -12,16 +12,24 @@ class LaunchList extends React.Component {
   render() {
     return (
       <Row gutter={16} type="flex" justify="space-around">
-        {this.props.data.map(flight => (
-          <Col xs={24} md={10}>
-            <Launch
-              mission={flight.mission_name}
-              rocket={flight.rocket.rocket_name}
-              site={flight.launch_site.site_name_long}
-              launchDate={flight.launch_date_local}
-            />
-          </Col>
-        ))}
+        {this.props.data.map(
+          flight =>
+            console.log(flight) || (
+              <Col
+                key={flight.mission_name}
+                xs={24}
+                md={10}
+                style={{ margin: "10px" }}
+              >
+                <Launch
+                  mission={flight.mission_name}
+                  rocket={flight.rocket}
+                  site={flight.launch_site.site_name_long}
+                  launchDate={flight.launch_date_local}
+                />
+              </Col>
+            )
+        )}
       </Row>
     );
   }
