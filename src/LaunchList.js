c0084c7c100a2fa32e413a85ccbbd9182f52bdfa
Florian Kapfenberger <florian@kapfenberger.me>
2018-10-18 13:47:25 +0200

    removed console.log

--- src/LaunchList.js
+++ src/LaunchList.js
@@ -12,24 +12,21 @@ class LaunchList extends React.Component {
   render() {
     return (
       <Row gutter={16} type="flex" justify="space-around">
-        {this.props.data.map(
-          flight =>
-            console.log(flight) || (
-              <Col
-                key={flight.mission_name}
-                xs={24}
-                md={10}
-                style={{ margin: "10px" }}
-              >
-                <Launch
-                  mission={flight.mission_name}
-                  rocket={flight.rocket}
-                  site={flight.launch_site.site_name_long}
-                  launchDate={flight.launch_date_local}
-                />
-              </Col>
-            )
-        )}
+        {this.props.data.map(flight => (
+          <Col
+            key={flight.mission_name}
+            xs={24}
+            md={10}
+            style={{ margin: "10px" }}
+          >
+            <Launch
+              mission={flight.mission_name}
+              rocket={flight.rocket}
+              site={flight.launch_site.site_name_long}
+              launchDate={flight.launch_date_local}
+            />
+          </Col>
+        ))}
       </Row>
     );
   }
